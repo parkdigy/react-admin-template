80cb2482dea7180bd8a8a02a9ea1f97722aea0d1
parkdigy <parkdigy@gmail.com>
2025-06-29 02:38:45 +0900

    Update

--- client/src/init/global/admin.ts
+++ client/src/init/global/admin.ts
@@ -1,8 +1,8 @@
-/* eslint-disable */
+/* eslint-disable no-var */
 declare global {
   var SUPER_ADMIN_GROUP_ID: number;
 }
-/* eslint-enable */
+/* eslint-enable no-var */
 
 globalThis.SUPER_ADMIN_GROUP_ID = 1;
 
