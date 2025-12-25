32738ccc8ca466d6a1331beca8bbc0ae415381ff
parkdigy <parkdigy@gmail.com>
2025-12-26 03:11:59 +0900

    Update

--- client/src/component/Dashboard/index.ts
+++ client/src/component/Dashboard/index.ts
@@ -1,5 +1,7 @@
 import l from '@loadable/component';
 
-const Dashboard = l(() => import('./Dashboard'));
+const Dashboard = l(() => import('./Dashboard'), loadable.options) as unknown as typeof import('./Dashboard').default;
 
 export { Dashboard };
+
+export default Dashboard;
